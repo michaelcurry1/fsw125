6c9d543e062efff4a51e9d56a4189b962a56b4a0
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-06-14 11:57:46 -0400

    newTry

---
+++ week1/math.js
@@ -0,0 +1,21 @@
+// math.js
+
+const sum =(a, b) => {
+    return a + b
+};
+module.exports = sum;
+
+const sub =(a, b) => {
+    return a + b
+};
+module.exports = sub;
+
+const mult =(a, b) => {
+    return a + b
+};
+module.exports = mult;
+
+const div =(a, b) => {
+    return a + b
+};
+module.exports = div;
---
+++ week1/math/app.js
@@ -0,0 +1,14 @@
+
+
+const sumFunc = require('./math')
+console.log(sumFunc)
+
+const subFunc = require('./math')
+console.log(subFunc)
+
+const multFunc = require('./math')
+console.log(multFunc)
+
+const divFunc = require('./math')
+console.log(divFunc)
+
