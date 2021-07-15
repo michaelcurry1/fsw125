776baf9a2fe5cbefd2916566ac8731651580a19c
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-07-15 12:13:00 -0400

    1st

---
+++ week6/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+
+app.use("/thingFinder",require('../thingFinder.js'))
+
+app.listen('3000', () => {
+    console.log('thing finder server.')
+})
---
+++ week6/thingFinder.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const thingFinder = express.Router();
+
+const fruit = [
+    {
+    type:"banana",
+    brand:" chicita",
+    price:5,
+ 
+},
+{
+    type:"orange",
+    brand:" sunkist",
+    price:7,
+    
+},
+{
+    type:"grapes",
+    brand:" purple hue",
+    price:15,
+    
+},
+{
+    type:"apples",
+    brand:" green acres",
+    price:12,
+    
+},
+]
+thingFinder.route('/').get( (req,res) =>{
+    res.send(fruit)
+})
+thingFinder.route('/:fruitType').get((req, res ) =>{
+    const fruit = req.params.fruitType
+    const foundfruit = fruit.find(fruit => fruit.type === fruitType)
+    res.send(foundfruit)
+
+})
+
+
+
+
+
+
+
+
+module.exports = thingFinder
